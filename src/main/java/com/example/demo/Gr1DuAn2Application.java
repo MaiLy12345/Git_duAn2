c870bf59cbe644d9e57e33486370364079f32015
MaiLy12345 <35954248+MaiLy12345@users.noreply.github.com>
2018-11-05 23:17:14 +0700

    caa

---
+++ src/main/java/com/example/demo/Gr1DuAn2Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gr1DuAn2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gr1DuAn2Application.class, args);
+	}
+}
---
+++ src/test/java/com/example/demo/Gr1DuAn2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Gr1DuAn2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
