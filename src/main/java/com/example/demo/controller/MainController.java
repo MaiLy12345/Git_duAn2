8072b0575b9fae5892135477d92aceae98173df9
khangnguyen324 <37128270+khangnguyen324@users.noreply.github.com>
2018-11-06 18:04:12 +0700

    Update src/main/java/com/example/demo/controller/MainController.java

---
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -0,0 +1,67 @@
+package com.example.demo.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.example.demo.dao.OrderDAO;
+import com.example.demo.dao.ProductDAO;
+import com.example.demo.entity.Product;
+import com.example.demo.form.CustomerForm;
+import com.example.demo.model.CartInfo;
+import com.example.demo.model.CustomerInfo;
+import com.example.demo.model.ProductInfo;
+import com.example.demo.pagination.PaginationResult;
+import com.example.demo.validator.CustomerFormValidator;
+
+import antlr.Utils;
+ 
+@Controller
+@Transactional
+public class MainController {
+ 
+    @Autowired
+    private OrderDAO orderDAO;
+ 
+    @Autowired
+    private ProductDAO productDAO;
+ 
+    @Autowired
+    private CustomerFormValidator customerFormValidator;
+ 
+    @InitBinder
+    public void myInitBinder(WebDataBinder dataBinder) {
+        Object target = dataBinder.getTarget();
+        if (target == null) {
+            return;
+        }
+        System.out.println("Target=" + target);
+ 
+        // Trường hợp update SL trên giỏ hàng.
+        // (@ModelAttribute("cartForm") @Validated CartInfo cartForm)
+        if (target.getClass() == CartInfo.class) {
+ 
+        }
+ 
+        // Trường hợp save thông tin khách hàng.
+        // (@ModelAttribute @Validated CustomerInfo customerForm)
+        else if (target.getClass() == CustomerForm.class) {
+            dataBinder.setValidator(customerFormValidator);
+        }
+ 
+    }
